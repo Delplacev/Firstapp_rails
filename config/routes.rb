618a6d71370c07f33a7efb00a2689c70c3c54f6e
Headoverz <djvivi59@gmail.com>
2016-09-21 16:42:36 +0200

    MAJ 21/09 16:42

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
   get '/contact', :to => 'pages#contact'
   get '/home',   :to => 'pages#home'
   get '/help',   :to => 'pages#help'
-  #match '/signup',    :to => ''
+  get '/signup',    :to => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
