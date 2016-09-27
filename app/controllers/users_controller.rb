4e17297539f6bb965355655c0484dd23ab66be3c
Virgile <djvivi59@gmail.com>
2016-09-27 09:30:52 +0200

    MAJ 27/09 9:30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def user_params
     params.require(:user).permit(:nom, :email, :password,
                                        :password_confirmation)
@@ -25,5 +27,27 @@ end
       @titre = "Inscription"
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @titre = "Édition profil"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profil actualisé."
+      redirect_to @user
+    else
+      @titre = "Édition profil"
+      render 'edit'
+    end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,15 @@ end
 def signed_in?
 !current_user.nil?
 end
+
+def deny_access
+    redirect_to signin_path, :notice => "Merci de vous identifier pour rejoindre cette page."
+  end
+
+  def redirect_back_or(default)
+  redirect_to(session[:return_to] || default)
+  clear_return_to
+end
   private
 
     def user_from_remember_token
@@ -30,4 +39,12 @@ end
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
     end
+
+    def store_location
+  session[:return_to] = request.fullpath
+end
+
+def clear_return_to
+  session[:return_to] = nil
+end
 end
